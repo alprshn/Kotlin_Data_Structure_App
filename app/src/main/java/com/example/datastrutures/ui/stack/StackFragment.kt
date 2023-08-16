fb54cd1eb8536470da5e966f7f4774ef24b4e878
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-08-16 14:56:26 +0300

    Complete Stack Algorithm

--- app/src/main/java/com/example/datastrutures/ui/stack/StackFragment.kt
+++ app/src/main/java/com/example/datastrutures/ui/stack/StackFragment.kt
@@ -3,9 +3,11 @@ package com.example.datastrutures.ui.stack
 import android.content.Intent
 import android.os.Bundle
 import android.text.Html
+import android.text.Spanned
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.datastrutures.databinding.FragmentStackBinding
@@ -16,9 +18,10 @@ class StackFragment : Fragment() {
     private var _binding: FragmentStackBinding? = null
 
 
-    private var popStorage  = ArrayList<Any>()
+    private var popStorage = ArrayList<Any>()
 
     var stack = Stack<Any>()
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -41,28 +44,25 @@ class StackFragment : Fragment() {
 
         }
         val markdownText = """
-           # Kotlin_Note_App V1
-
-<h2>Documentation Dokka</h2>
-Documentation rules for Kotlin in this link https://kotlinlang.org/docs/kotlin-doc.html
-<h3>Setup Dokka</h3>
-
-This will be written in build.gradle(Module:app).
-```
-plugins {
-    id("org.jetbrains.dokka") version "1.8.20"
-}
-```
-
-and to start it in terminal.
-```
- ./gradlew dokkaHtml  
-```
-
-
+        <center><h1>Stack Data Structures</h1><center><br>
+        <p>The stack data structure is identical, in concept, to a physical stack of objects. When you add an item to a stack, you place it on top of the stack. When you remove an item from a stack, you always remove the top-most item.</p>
+        <br>       
+        <h3>Stack operations</h3><br>
+        <p>Stacks are useful, and also exceedingly simple. The main goal of building a stack is to enforce how you access your data. If you had a tough time with the linked list concepts, you’ll be glad to know that stacks are comparatively trivial.</p>
+        There are only two essential operations for a stack:<br>
+        <li><strong>push:</strong> Adding an element to the top of the stack.</li><br>
+        <li><strong>pop:</strong> Removing the top element of the stack.</li><br>
+        <hr>
+        <p>This means that you can only add or remove elements from one side of the data structure. In computer science, a stack is known as the LIFO (last-in first-out) data structure. Elements that are pushed in last are the first ones to be popped out.</p><br>
         """.trimIndent()
 
-        val htmlText = Html.fromHtml(markdownText, Html.FROM_HTML_MODE_COMPACT)
+        val htmlText: Spanned = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            HtmlCompat.fromHtml(markdownText, HtmlCompat.FROM_HTML_MODE_COMPACT)
+        } else {
+            @Suppress("DEPRECATION")
+            Html.fromHtml(markdownText)
+        }
+
         binding.stackTest.text = htmlText
 
 
