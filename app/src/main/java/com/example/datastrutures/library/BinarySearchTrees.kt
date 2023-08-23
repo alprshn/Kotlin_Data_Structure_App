cb06af2dd94d67abc5442c8908bce5e9a08aa053
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-08-23 21:57:15 +0300

    Added BST Algorithm

--- app/src/main/java/com/example/datastrutures/library/BinarySearchTrees.kt
+++ app/src/main/java/com/example/datastrutures/library/BinarySearchTrees.kt
@@ -1,15 +1,34 @@
 package com.example.datastrutures.library
 
+import android.provider.ContactsContract.Intents.Insert
 
-class Node(     /* Node data structure for binary search tree */
-                var data: String,
-                var leftChild: Node? = null,
-                var rightChild: Node? = null
-)
+
+class Node(item: Int) {     /* Node data structure for binary search tree */
+    var data: Int = item
+    var leftChild: Node? = null
+    var rightChild: Node? = null
+
+}
+
+var root: Node? = null
 
 class BinarySearchTrees {
 
-    fun Insert(node: Node?, data: String) {
+    fun Insert(data: Int) {
+        root = InsertData(root, data);
+    }
 
+    fun InsertData(root: Node?, data: Int): Node {
+        if (root == null) {
+            root = Node(data)
+            return root
+        }
+        if (data < root.data) {
+            root.leftChild = InsertData(root.leftChild, data)
+        } else if (data > root.data) {
+            root.rightChild = InsertData(root.rightChild, data)
+        }
+
+        return root
     }
 }
