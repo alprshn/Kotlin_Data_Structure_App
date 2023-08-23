ec4ee625978418950683473b619f98ce8caab260
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-08-23 16:45:19 +0300

    Added BST Algorithm

---
+++ app/src/main/java/com/example/datastrutures/library/BinarySearchTrees.kt
@@ -0,0 +1,15 @@
+package com.example.datastrutures.library
+
+
+class Node(     /* Node data structure for binary search tree */
+                var data: String,
+                var leftChild: Node? = null,
+                var rightChild: Node? = null
+)
+
+class BinarySearchTrees {
+
+    fun Insert(node: Node?, data: String) {
+
+    }
+}
