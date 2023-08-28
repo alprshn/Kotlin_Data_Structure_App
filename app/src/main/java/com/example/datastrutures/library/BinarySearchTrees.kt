36d5290f444544d73ab9aa434858039113dd9e78
Alper Sahin <alper_sahin20@erdogan.edu.tr>
2023-08-28 13:26:47 +0300

    Edit BST Algorithm

--- app/src/main/java/com/example/datastrutures/library/BinarySearchTrees.kt
+++ app/src/main/java/com/example/datastrutures/library/BinarySearchTrees.kt
@@ -1,7 +1,5 @@
 package com.example.datastrutures.library
 
-import android.provider.ContactsContract.Intents.Insert
-
 
 class Node(item: Int) {     /* Node data structure for binary search tree */
     var data: Int = item
@@ -27,7 +25,58 @@ class BinarySearchTrees {
         } else if (data > root.data) {
             root.rightChild = InsertData(root.rightChild, data)
         }
-
         return root
     }
+
+    fun inorder() {
+        InorderRec(root)
+    }
+
+    fun InorderRec(root: Node?) {
+        if (root != null) {
+            InorderRec(root.leftChild)
+            ///System.out.print(root.data + " -> ") data gelicek
+            InorderRec(root.rightChild)
+        }
+    }
+
+    fun DeleteData(data: Int) {
+        root = DeleteRec(root, data)
+    }
+
+    fun DeleteRec(root: Node?, data: Int): Node? {
+        if (root == null) {
+            return root
+        }
+        if (data < root.data)
+            root.leftChild = DeleteRec(root.leftChild, data)
+        else if (data > root.data)
+            root.rightChild = DeleteRec(root.rightChild, data)
+        else {
+            // If the node is with only one child or no child
+            if (root.leftChild == null)
+                return root.rightChild
+            else if (root.rightChild == null)
+                return root.leftChild
+
+            // If the node has two children
+            // Place the inorder successor in position of the node to be deleted
+            root.data = minValue(root.rightChild);
+
+            // Delete the inorder successor
+            root.rightChild = DeleteRec(root.rightChild, root.data);
+        }
+        return root;
+    }
+
+    fun minValue(root: Node?): Int {
+        var minv = root?.data
+        var current = root
+        while (current?.leftChild != null) {
+            minv = current?.leftChild!!.data
+            current = current?.leftChild
+        }
+        return minv!!
+    }
+
 }
